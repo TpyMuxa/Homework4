a33e4a4ca6380f634bf4666017a7fe2fd6d7f45f
TpyMuxa <tpymuxa1133@gmail.com>
2023-08-29 20:41:41 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1");
+
+        int ageAdult = 18;
+        int ageMinor = 17;
+
+        if (ageAdult >= 18) {
+
+            System.out.println("Если возраст человека равен " + ageAdult + " ,то он совершеннолетний");
+        }
+        if (ageMinor < 18) {
+
+            System.out.println("Если возраст человека равен " + ageMinor + " ,то он не достиг совершеннолетия," +
+                    "нужно немного подождать");
+        }
+
     }
 }
